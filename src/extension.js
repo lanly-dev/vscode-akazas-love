f5d3b29d36519a53bab492bc34e252db0fa2d88a
Lan Ly <lanly-dev@users.noreply.github.com>
2025-09-19 19:51:29 -0700

    reset

--- src/extension.js
+++ src/extension.js
@@ -1,33 +1,34 @@
-const vscode = require('vscode')
-const MusicalTyping = require('./MusicalTyping')
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
 
-let musicalTyping
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
 
+/**
+ * @param {vscode.ExtensionContext} context
+ */
 function activate(context) {
-  console.log('Akaza\'s Love - Musical Typing extension is now active!')
 
-  // Initialize musical typing
-  musicalTyping = new MusicalTyping(context)
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "akazas-love" is now active!');
 
-  // Register commands
-  const toggleCommand = vscode.commands.registerCommand('akazas-love.toggleMusic', () => {
-    musicalTyping.toggleEnabled()
-  })
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposable = vscode.commands.registerCommand('akazas-love.helloWorld', function () {
+    // The code you place here will be executed every time your command is executed
 
-  const reloadCommand = vscode.commands.registerCommand('akazas-love.reloadMidi', () => {
-    musicalTyping.reloadMidi()
-  })
+    // Display a message box to the user
+    vscode.window.showInformationMessage(`Hello World from akaza's love!`);
+  });
 
-  context.subscriptions.push(toggleCommand, reloadCommand)
+  context.subscriptions.push(disposable);
 }
 
-function deactivate() {
-  if (musicalTyping) {
-    musicalTyping.dispose()
-    musicalTyping = null
-  }
-  console.log('Akaza\'s Love - Musical Typing extension deactivated')
-}
+// This method is called when your extension is deactivated
+function deactivate() { }
 
 module.exports = {
   activate,
